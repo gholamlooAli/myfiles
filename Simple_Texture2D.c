3137875d51bb8921f3b586fcf242ace9ec06f9d8
ali gholamloo <gholamlooali@gmail.com>
2017-02-17 16:59:29 -0500

    Add files via upload
    changes on feb17

--- Simple_Texture2D.c
+++ Simple_Texture2D.c
@@ -17,7 +17,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "esUtil.h"
-
+//#include <GL/glut.h>
+GLint im_width,im_height;
+GLfloat image_width,image_height;
+GLfloat	image[2];//={640.0f,480.0f};
 typedef struct
 {
    // Handle to a program object
@@ -35,6 +38,14 @@ typedef struct
 
 } UserData;
 
+void check_error(char *label)
+{
+	GLenum error;
+	while ((error= glGetError()) != GL_NO_ERROR)
+	{
+		fprintf(stderr, "%s error detected: \n",label);  
+	}
+}
 
 char* esLoadRaw ( char *fileName, int width, int height )
 {
@@ -69,7 +80,6 @@ GLuint LoadTexture ( char *fileName ,int width ,int height)
    //int width,height;
    char *buffer = esLoadRaw ( fileName, width, height );
    GLuint texId;
-
    if ( buffer == NULL )
    {
       esLogMessage ( "Error loading (%s) image.\n", fileName );
@@ -135,9 +145,13 @@ int Init ( ESContext *esContext )
 {
    esContext->userData = malloc(sizeof(UserData));	
    UserData *userData = esContext->userData;
+   GLint imwLoc;
+   GLint imhLoc;	
+   
    GLbyte vShaderStr[] =  
       "attribute vec4 a_position;   \n"
       "attribute vec2 a_texCoord;   \n"
+//"uniform float image_width;			\n"
       "varying vec2 v_texCoord;     \n"
       "void main()                  \n"
       "{                            \n"
@@ -149,9 +163,11 @@ int Init ( ESContext *esContext )
       "precision mediump float;                            \n"
       "varying vec2 v_texCoord;                            \n"
       "uniform sampler2D s_texture;                        \n"
+	"uniform vec2 uimage_width;			\n"
+//	"uniform vec1 image_height;			\n"
       "void main()                                         \n"
-	"#define IMAGE_WIDTH 640.0			\n"
-	"#define IMAGE_HEIGHT 480.0			\n"
+	
+
       "{                                                   \n"
       //"  gl_FragColor = texture2D( s_texture, v_texCoord );\n"
 	  "float red, green, blue;       \n"
@@ -164,26 +180,27 @@ int Init ( ESContext *esContext )
 	  "float x, y;		       \n"
 	  "float u, v;		       \n"
 	  "vec3 yuv;		       \n"
+	  "vec4 res;		       \n"
 	// note: pixelx, pixely are 0.0 to 1.0 so "next pixel horizontally"
   	//  is not just pixelx + 1; rather pixelx + texel_width.
 
 	  "pixelx = v_texCoord.x;	\n"
 	  "pixely = v_texCoord.y;	\n"
-	  "texture_width=640.0;	\n"
-	  "texture_height=480.0;	\n"
-	  "texel_width=1.0/IMAGE_WIDTH; \n"
-	  "texel_height=1.0/IMAGE_HEIGHT; \n"	
-	  "x = pixelx - (IMAGE_WIDTH * 0.5) / texture_width;	\n"
-	  "y = pixely - (IMAGE_HEIGHT * 0.5) / texture_height;	\n"
+	  "texture_width=uimage_width.x;	\n"
+	  "texture_height=uimage_width.y;	\n"
+	  "texel_width=1.0/texture_width; \n"
+	  "texel_height=1.0/texture_height; \n"	
+	  //"x = pixelx - (IMAGE_WIDTH * 0.5) / texture_width;	\n"
+	  //"y = pixely - (IMAGE_HEIGHT * 0.5) / texture_height;	\n"
 
-	  "u = x*x - y*y;	\n"
-	  "v = 2.0*x*y;	\n"
+	  //"u = x*x - y*y;	\n"
+	  //"v = 2.0*x*y;	\n"
 
-	  "u = u + (IMAGE_WIDTH * 0.5) / texture_width;		\n"
-	  "v = v + (IMAGE_HEIGHT * 0.5) / texture_height;	\n"
+	  //"u = u + (IMAGE_WIDTH * 0.5) / texture_width;		\n"
+	  //"v = v + (IMAGE_HEIGHT * 0.5) / texture_height;	\n"
 	  
-	  "pixelx = clamp(u, 0.0, (IMAGE_WIDTH - 1.0) / texture_width);		\n"
-	  "pixely = clamp(v, 0.0, (IMAGE_HEIGHT - 1.0) / texture_height);	\n"
+	  //"pixelx = clamp(u, 0.0, (IMAGE_WIDTH - 1.0) / texture_width);		\n"
+	  //"pixely = clamp(v, 0.0, (IMAGE_HEIGHT - 1.0) / texture_height);	\n"
 	  // if pixelx is even, then that pixel contains [Y U] and the 
 	  //    next one contains [Y V] -- and we want the V part.
 	  // if  pixelx is odd then that pixel contains [Y V] and the 
@@ -192,32 +209,32 @@ int Init ( ESContext *esContext )
 	  // note: only valid for images whose width is an even number of
 	  // pixels
   
-  	  "xcoord = floor (pixelx * texture_width);	\n"
+  	  "xcoord = floor (v_texCoord.x * texture_width);	\n"
   
 	  "luma_chroma = texture2D(s_texture, v_texCoord);	\n"
   
 	  // just look up the brightness
-	  //"luma = (luma_chroma.a - 0.0625) * 1.1643;	\n"
-	  "luma=luma_chroma.a;	\n"
-	  "if (0.0 == mod(v_texCoord.x , 2.0)) \n"	// even
+	  "luma = (luma_chroma.a - 0.0625) * 1.1643;	\n"
+	  //"luma=luma_chroma.a;	\n"
+	  "if (0.0 == mod(xcoord , 2.0)) \n"	// even
 	   " {				\n"
-	   "   chroma_u = luma_chroma.b;	\n"
+	   "   chroma_u = luma_chroma.r;	\n"
 //	   "   chroma_v = texture2D(s_texture,vec2(pixelx + texel_width, pixely)).r;	\n"
 	   "   chroma_v = texture2D(s_texture,vec2(v_texCoord.x + texel_width, v_texCoord.y)).r;	\n"
 	   " }	\n"
 	  "else \n"// odd
 	   " {	\n"
 	   "   chroma_v = luma_chroma.r;	\n"
-	   "   chroma_u = texture2D(s_texture,vec2(v_texCoord.x - texel_width, v_texCoord.y)).b; 	\n"    
+	   "   chroma_u = texture2D(s_texture,vec2(v_texCoord.x - texel_width, v_texCoord.y)).r; 	\n"    
 //	   "   chroma_u = texture2D(s_texture,vec2(pixelx - texel_width, pixely)).r; 	\n"    
 	   "}	\n"
-	  //"chroma_u = chroma_u - 0.5;	\n"
+	  "chroma_u = chroma_u - 0.5;	\n"
 	  "chroma_v = chroma_v - 0.5;	\n"
 
-	  "red = luma + 1.402 * chroma_v;	\n"
-	  "green = luma - 0.3441 * chroma_u - 0.7141 * chroma_v;	\n"
-	  "blue = luma + 1.772 * chroma_u;	\n"
-
+	  "res.r = luma + 1.5958 * chroma_v;	\n"
+	  "res.g = luma - 0.39173 * chroma_u - 0.8129 * chroma_v;	\n"
+	  "res.b = luma + 2.017 * chroma_u;	\n"
+	  "res.a = 1.0;				\n"
 //	  "red = chroma_u ;	\n"
 //	  "green = 0.0;	\n"
 //	  "blue = 0.0 ;	\n"
@@ -225,7 +242,7 @@ int Init ( ESContext *esContext )
 //	  "red = luma ;	\n"
 //	  "green = luma;	\n"
 //	  "blue = luma ;	\n"
-	  "gl_FragColor = vec4(red, green, blue, 1.0);	\n"
+	  "gl_FragColor = clamp(res, vec4(0), vec4(1));	\n"
       "}                                                   \n";
 
 
@@ -244,9 +261,35 @@ int Init ( ESContext *esContext )
    // Get the sampler location
    userData->samplerLoc = glGetUniformLocation ( userData->programObject, "s_texture" );
 
+   // Get the image_width location
+   imwLoc = glGetUniformLocation ( userData->programObject, "uimage_width" );
+   if(-1== imwLoc)
+   {
+	fprintf(stderr, "warning imwLoc error\n");
+	check_error("war imwLoc");
+   }
+   else
+   {
+	glUniform2fv(imwLoc,1,image);
+	check_error("war after imwLoc");	
+   }   
+			
+   // Get the image_height location
+ /*  imhLoc = glGetUniformLocation ( userData->programObject, "image_height" );
+   if(-1== imhLoc)
+   {
+	fprintf(stderr, "warning imhLoc error\n");
+		check_error("war imhLoc");
+   }
+   else
+   {
+	glUniform1fv(imhLoc, im_height);
+		check_error("war after imhLoc");
+   }  
+ */
    // Load the texture
    //userData->textureId = CreateSimpleTexture2D ();
-   userData->textureId=LoadTexture ("/home/linaro/frame6.raw",640,480);
+   userData->textureId=LoadTexture ("/home/linaro/frame6.raw",im_width,im_height);
    glClearColor ( 0.0f, 0.0f, 0.0f, 0.0f );
    return GL_TRUE;
 }
@@ -321,8 +364,15 @@ int main ( int argc, char *argv[] )
 
    esInitContext ( &esContext );
    esContext.userData = &userData;
+	im_width=640;
+	im_height=480;
+	image[0]=(GLfloat)im_width;
+	image[1]=(GLfloat)im_height;
+	//image_width=640.0;//(float)im_width;
 
-   esCreateWindow ( &esContext, "Simple Texture 2D",640,480,/*320, 240,*/ ES_WINDOW_RGB );
+	//image_height=480.0;//(float)im_height;
+
+   esCreateWindow ( &esContext, "Simple Texture 2D",im_width,im_height/*640, 480*/, ES_WINDOW_RGB );
 
    if ( !Init ( &esContext ) )
       return 0;
